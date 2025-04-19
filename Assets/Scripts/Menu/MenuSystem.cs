d7c80c01e1717e6627621ec0f0dbbd3fa08a1c19
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-04-18 23:14:34 -0500

    menu listo y hecho para probarse

---
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+public class MenuSystem : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Salir()
+    {
+        Console.WriteLine("Saliendo del juego...");
+        Application.Quit();
+    }
+}
