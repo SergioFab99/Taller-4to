86c14879950f41fbff8b5d36a92f254f4b7b8128
RuinedKing08 <newapheliosplayer@gmail.com>
2025-04-19 12:05:24 -0500

    arreglando

--- Assets/Scripts/suputa.cs
+++ Assets/Scripts/suputa.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class suputa : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset = new Vector3(0f, 3f, -6f);
-    public float rotationSpeed = 3f;
+    public Transform target; 
+    public Vector3 offset = new Vector3(0f, 2f, -5f);
+    public float sensitivityX = 3f;
+    public float sensitivityY = 2f;
+    public float minY = -30f;
+    public float maxY = 60f;
 
-    private float currentYaw = 0f;
+    private float yaw = 0f;
+    private float pitch = 15f;
 
     void LateUpdate()
     {
-        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
-        currentYaw += mouseX;
+        yaw += Input.GetAxis("Mouse X") * sensitivityX;
+        pitch -= Input.GetAxis("Mouse Y") * sensitivityY;
+        pitch = Mathf.Clamp(pitch, minY, maxY);
 
-        Quaternion rotation = Quaternion.Euler(0f, currentYaw, 0f);
-        Vector3 rotatedOffset = rotation * offset;
+        Quaternion rotation = Quaternion.Euler(pitch, yaw, 0f);
+        Vector3 cameraPos = target.position + rotation * offset;
 
-        transform.position = target.position + rotatedOffset;
-        transform.LookAt(target.position + Vector3.up * 1.5f);
+        transform.position = cameraPos;
+        transform.LookAt(target.position + Vector3.up * 1.5f); 
+    }
+
+    public Vector3 GetFlatForward()
+    {
+        Vector3 forward = transform.forward;
+        forward.y = 0;
+        return forward.normalized;
+    }
+
+    public Vector3 GetLookDirection()
+    {
+        return transform.forward.normalized;
     }
 }
--- Assets/side scheme/bar.cs
+++ Assets/side scheme/bar.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class bar : MonoBehaviour
 {
-    public Image oxygenImage;   
+    public Image oxygenImage;
+    public GameObject cat;
     public float maxOxygen = 100f;
     public float drainRate = 5f; 
 
@@ -13,6 +15,7 @@ public class bar : MonoBehaviour
     {
         currentOxygen = maxOxygen;
         UpdateBar();
+        cat.SetActive(false);
     }
 
     void Update()
@@ -20,6 +23,16 @@ public class bar : MonoBehaviour
         currentOxygen -= drainRate * Time.deltaTime;
         currentOxygen = Mathf.Clamp(currentOxygen, 0f, maxOxygen);
         UpdateBar();
+
+        if(currentOxygen <= 0)
+        {
+            cat.SetActive(true);
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 
     public void RefillOxygen(float amount)
