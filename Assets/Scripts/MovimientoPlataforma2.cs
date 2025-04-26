fbc77d1dd34a206665e6da0b1b8ad08be6029ae5
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-04-26 10:00:54 -0500

    movimiento de plataformas

---
+++ Assets/Scripts/MovimientoPlataforma2.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MovimientoPlataforma2: MonoBehaviour
+{
+    private Vector3 startPosition;
+    public float range = 3f;
+    public float speed = 2f;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float offset = Mathf.PingPong(Time.time * speed, range * 2) - range;
+        transform.position = new Vector3(startPosition.x + offset, startPosition.y, startPosition.z);
+    }
+}
