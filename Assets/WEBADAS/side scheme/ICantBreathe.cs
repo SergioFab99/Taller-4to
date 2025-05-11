a8935fe3ce83d458ecb49db1d6ccaf576a167ac2
FREEZEXD666 <146866716+FREEZEXD666@users.noreply.github.com>
2025-05-11 11:41:23 -0500

    AddedBubbleSoundEffect

--- Assets/WEBADAS/side scheme/ICantBreathe.cs
+++ Assets/WEBADAS/side scheme/ICantBreathe.cs
@@ -3,14 +3,28 @@ using UnityEngine;
 public class ICantBreathe : MonoBehaviour
 {
     public float refillAmount = 100f;
+    public AudioClip PopSound;
+    private AudioSource audioSource;
+    public float soundVolume = 0.5f;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.playOnAwake = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         bar bb = FindFirstObjectByType<bar>();
         if (bb != null)
         {
+            audioSource.PlayOneShot(PopSound);
             bb.RefillOxygen(refillAmount);
-            Destroy(gameObject); 
+            Destroy(gameObject);
         }
     }
 }
