0ddc906ad576aa743582118c79fa1b8355b227cb
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-05-12 17:30:32 -0500

    koi listo para ser usado en el nivel 1

--- Assets/Scripts/Scripts Mateo/ThirdPersonCameraController.cs
+++ Assets/Scripts/Scripts Mateo/ThirdPersonCameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class suputa : MonoBehaviour
+public class ThirdPersonCameraController: MonoBehaviour
 {
     public Transform target; 
     public Vector3 offset = new Vector3(0f, 2f, -5f);
