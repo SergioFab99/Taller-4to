4352f1870eecf0192c4308877362d2b17e63ff9a
JPaco02 <jjpp_02_07@hotmail.com>
2025-07-05 23:48:33 -0500

    Menu - Ajustes en Botones y logo

--- Assets/Scripts/Menu/MenuSystem.cs
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -4,10 +4,22 @@ using UnityEngine.SceneManagement;
 public class MenuSystem : MonoBehaviour
 {
     [SerializeField] private string nombreEscenaJuego = "Nivel 1"; // Cambia esto por el nombre real de tu escena
+    [SerializeField] private string nombreEscenaJuego1 = "Creditos";
+     [SerializeField] private string nombreEscenaJuego2;
 
     public void Jugar()
     {
         SceneManager.LoadScene(nombreEscenaJuego);
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene(nombreEscenaJuego1);
+    }
+
+    public void Ranking()
+    {
+        SceneManager.LoadScene(nombreEscenaJuego2);
     }
 
     public void Salir()
