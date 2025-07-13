d7f06735e7b9a873de9c2d8909a51fd528c9a72b
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-07-13 17:32:10 -0500

    el timer se para cuando mueres
    ahora el timer se detiene cuando mueres

--- Assets/Scripts/Scripts Sergio/Timer.cs
+++ Assets/Scripts/Scripts Sergio/Timer.cs
@@ -1,57 +1,48 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.UIElements;  // Necesario para TextMeshPro
 
 public class Timer : MonoBehaviour
 {
     public static float Timevalue;
     public TMP_Text timerText;
+    public PlayerInputHandler playerInputHandler;
 
-    // Variables del temporizador
     private float timeElapsed = 0f;
     private bool isTiming = false;
 
-    // Se ejecuta cuando la escena comienza
     void Start()
     {
-        // Iniciar el timer automáticamente al cargar la escena
         StartTimer();
     }
 
-    // Iniciar el timer
     public void StartTimer()
     {
-        // Reinicia el tiempo
         timeElapsed = 0f;
-        // Inicia el conteo
-        isTiming = true;   
+        isTiming = true;
     }
 
-    // Detener el timer
     public void StopTimer()
     {
-        isTiming = false;  // Detiene el conteo
+        isTiming = false;
     }
 
-    // Reiniciar el timer
     public void ResetTimer()
     {
-        // Resetea el tiempo
-        timeElapsed = 0f;  
-        // Muestra el tiempo reseteado
-        timerText.text = "0.00"; 
+        timeElapsed = 0f;
+        timerText.text = "0.00";
     }
 
-    // Update se llama una vez por frame
     void Update()
     {
         if (isTiming)
         {
-            // Aumenta el tiempo por el tiempo transcurrido desde el último frame
             timeElapsed += Time.deltaTime;
-            // Muestra el tiempo con 2 decimales
             timerText.text = timeElapsed.ToString("F2");
+            Timer.Timevalue = timeElapsed;
+            if (playerInputHandler.isFuckingDead)
+            {
+                StopTimer();
+            }
         }
-        Timevalue = timeElapsed;  // Actualiza la variable estática Timevalue
     }
 }
