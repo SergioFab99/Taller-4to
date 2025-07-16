1a6d4445e203dd40717e2a0b053bfd0d7012e1f5
RuinedKing08 <newapheliosplayer@gmail.com>
2025-07-15 21:14:24 -0500

    dadawdsa

--- Assets/Scripts/Scripts Sergio/DeadZone.cs
+++ Assets/Scripts/Scripts Sergio/DeadZone.cs
@@ -4,22 +4,22 @@ public class DeadZoneTrigger : MonoBehaviour
 {
     // Etiqueta del jugador
     [SerializeField] private string playerTag = "Player";
+    public PlayerInputHandler dead;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag))
         {
-            // Simula presionar la tecla R
-            PressRKey();
+            dead.isFuckingDead = false;
         }
     }
 
-    private void PressRKey()
-    {
-        // Si tienes lógica atada a Input.GetKeyDown(KeyCode.R), puedes moverla aquí.
-        // Por ejemplo, recargar la escena actual:
-        UnityEngine.SceneManagement.SceneManager.LoadScene(
-            UnityEngine.SceneManagement.SceneManager.GetActiveScene().name
-        );
-    }
+    //private void PressRKey()
+    //{
+    //    // Si tienes lógica atada a Input.GetKeyDown(KeyCode.R), puedes moverla aquí.
+    //    // Por ejemplo, recargar la escena actual:
+    //    UnityEngine.SceneManagement.SceneManager.LoadScene(
+    //        UnityEngine.SceneManagement.SceneManager.GetActiveScene().name
+    //    );
+    //}
 }
